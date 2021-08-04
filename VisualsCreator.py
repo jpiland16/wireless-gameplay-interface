28917f27c0c0b4a784e24ac85547fa003a6519f3
Jonathan Piland <jonathan.piland@duke.edu>
2021-08-04 16:07:58 -0400

    check for folder existence

--- VisualsCreator.py
+++ VisualsCreator.py
@@ -1,4 +1,4 @@
-import pickle
+import pickle, os
 
 from Testing import *
 from Visuals import *
@@ -6,13 +6,18 @@ from Visuals import *
 FILENAME = "similarity-test.pkl"
 
 # The below (line 9) should be unique, otherwise you will overwrite 
-# one of your other sites! Also, be sure that the 'html' folder exists 
-# and that the <SITE_NAME> folder exists, inside of the 'html' folder.
+# one of your other sites! 
 SITE_NAME = "similarity-tests"
 
 FOLDER = "html/" + SITE_NAME + "/"
 
 ##########################################################################
+
+if not os.path.exists('html'):
+    os.mkdir('html')
+
+if not os.path.exists('html/' + SITE_NAME):
+    os.mkdir('html/' + SITE_NAME)
 
 results = pickle.load(open(FILENAME, "rb"))
 
